5af0b689b8095736999a0af2f944a04a6f3c13b1
mfatur464 <mfathur464@gmail.com>
2026-03-13 15:15:52 +0700

    Add files via upload

---
+++ from datetime import datetime.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+class Book:
+    def __init__(self, idx, title, author):
+        self.__id = idx
+        self.__title = title
+        self.author = author
+        self.last_updated = datetime.now()
+
+    def get_id(self):
+        return self.__id
+
+    def get_title(self):
+        return self.__title
+
+    def set_title(self, new_title):
+        self.__title = new_title
